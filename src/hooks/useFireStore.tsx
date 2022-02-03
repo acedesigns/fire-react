1d39e7ccf1ab48811989fbcbdb525053097d03fc
Ace Designs <acedesigns123@gmail.com>
2022-02-03 20:05:55 +0200

    Added real time db lister

--- src/hooks/useFireStore.tsx
+++ src/hooks/useFireStore.tsx
@@ -9,26 +9,38 @@
 
 import { useEffect, useState } from 'react';
 import { app } from '../firebase/config';
-import { getDocs, getFirestore, collection, doc, onSnapshot } from "firebase/firestore"; 
+import { getDocs, getFirestore, collection, doc, onSnapshot, orderBy } from "firebase/firestore"; 
 
 const useFireStore = (coll: string) => {
 	const [docs, setDocs] = useState(Array);
 
 	useEffect(() => {
 		const dataBase = getFirestore(app);
-
+		
+		// Old way of getting Documents
+		/*
 		async function fetchMyImages() {
-			let myDocuments: Array<any> = [];
 			const querySnapshot = await getDocs(collection(dataBase, coll));
-			
+			let myDocuments: Array<any> = [];
 			querySnapshot.forEach((doc) => {
-				//sconsole.log(`${doc.id} `);
-				//console.log( doc.data());
 				myDocuments.push({...doc.data(), id: doc.id})
 			});
 			setDocs(myDocuments);
 		};
 		fetchMyImages();
+		*/
+
+		// Get Real-time db results as they happen
+		const unsub = onSnapshot(
+			collection (dataBase, "images"), 
+			//.orderBy('createdAt', 'desc')
+			(snap) => {
+				setDocs(snap.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+			},
+			(err) => {},
+		);
+
+		return () => unsub();
 		
 	}, [coll]);
 
